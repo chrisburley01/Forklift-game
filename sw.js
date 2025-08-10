95fd8182c0dac7d6cdd6f3da70dc7990f3ef23f7
chrisburley01 <chrisburley01@gmail.com>
2025-08-10 14:24:00 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = "forklift-v1";
-const ASSETS = ["./", "./index.html", "./manifest.json"];
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
@@ -19,4 +19,4 @@ self.addEventListener("fetch", (e) => {
   e.respondWith(
     caches.match(e.request).then((resp) => resp || fetch(e.request))
   );
-}); 
+});
